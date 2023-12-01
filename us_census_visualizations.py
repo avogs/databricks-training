ee868726519584820b478a6885d6a375b9a4ed14
avogs <avogs>
2023-12-01 23:40:23 +0000

    new things on the census dataset
    added a visualization which shows the count of zipcodes with a population density greater than 800 for each state

--- us_census_visualizations.py
+++ us_census_visualizations.py
@@ -19,3 +19,17 @@
 # MAGIC WHERE state_id NOT IN ('AS', 'GU', 'MP', 'PR', 'VI')
 # MAGIC GROUP BY state
 # MAGIC ORDER BY state;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT state_id AS state, SUM(population) as population
+# MAGIC FROM uszips_delta_unmanaged
+# MAGIC WHERE state_id NOT IN ('AS', 'GU', 'MP', 'PR', 'VI')
+# MAGIC GROUP BY state
+# MAGIC ORDER BY state;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT state_id, count(density) from uszips_delta_unmanaged WHERE density > 800 DEFAULT 0 GROUP BY state_id;
